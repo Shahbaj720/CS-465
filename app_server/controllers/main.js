2e407f1a3a7cb9d2411bbd849791ec32e6cae7ce
Shahbaj Singh <shahbajsidhu720@gmail.com>
2026-05-23 00:15:06 -0700

    Module 3: Dynamic JSON data rendering with HBS

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -1,9 +1,19 @@
+const fs = require('fs');
+const path = require('path');
+
+const trips = JSON.parse(
+  fs.readFileSync(path.join(__dirname, '../data/trips.json'), 'utf8')
+);
+
 const index = (req, res) => {
-  res.render('index');
+  res.render('index', { title: 'Travlr Getaways' });
 };
 
 const travel = (req, res) => {
-  res.render('travel');
+  res.render('travel', { 
+    title: 'Travlr Getaways - Travel',
+    trips: trips 
+  });
 };
 
 module.exports = { index, travel };
