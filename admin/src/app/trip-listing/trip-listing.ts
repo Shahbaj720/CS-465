3a1451d4294388a5580e4a3d1eaba50f159267e2
Shahbaj Singh <shahbajsidhu720@gmail.com>
2026-06-09 17:24:37 -0700

    Fix: force change detection in trip-listing component

--- admin/src/app/trip-listing/trip-listing.ts
+++ admin/src/app/trip-listing/trip-listing.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { Router } from '@angular/router';
 import { TripService, Trip } from '../services/trip';
 
@@ -14,7 +14,8 @@ export class TripListingComponent implements OnInit {
 
   constructor(
     private tripService: TripService,
-    private router: Router
+    private router: Router,
+    private cdr: ChangeDetectorRef
   ) {}
 
   ngOnInit(): void {
@@ -22,23 +23,25 @@ export class TripListingComponent implements OnInit {
   }
 
   getTrips(): void {
-  this.tripService.getTrips().subscribe({
-    next: (trips: Trip[]) => {
-      console.log('Trips received:', trips);
-      this.trips = trips;
-      this.message = `There are ${trips.length} trips available`;
-    },
-    error: (err: any) => {
-      console.error('Error fetching trips:', err);
-      this.message = 'Error fetching trips';
-    }
-  });
-}
+    this.tripService.getTrips().subscribe({
+      next: (trips: Trip[]) => {
+        console.log('Trips received:', trips);
+        this.trips = [...trips];
+        this.message = `There are ${trips.length} trips available`;
+        this.cdr.detectChanges();
+      },
+      error: (err: any) => {
+        console.error('Error fetching trips:', err);
+        this.message = 'Error fetching trips';
+      }
+    });
+  }
+
   addTrip(): void {
     this.router.navigate(['add-trip']);
   }
 
   editTrip(trip: Trip): void {
-  this.router.navigate(['edit-trip', trip.code]);
+    this.router.navigate(['edit-trip', trip.code]);
   }
 }
